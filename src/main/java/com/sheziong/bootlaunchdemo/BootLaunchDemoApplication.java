47122bc9cf36b93eae79f005af62199034faf5c5
zhifengShe <zhifengshe0920@163.com>
2020-04-24 22:10:36 +0800

    1-3 hello world

---
+++ src/main/java/com/sheziong/bootlaunchdemo/BootLaunchDemoApplication.java
@@ -0,0 +1,18 @@
+package com.sheziong.bootlaunchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
+
+
+@SpringBootApplication
+public class BootLaunchDemoApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BootLaunchDemoApplication.class, args);
+    }
+
+}
